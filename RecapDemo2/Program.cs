8bbadb0958cb99f7940a67030b8b525baaa61e82
memoa <memoa@DESKTOP-CTHT4UQ>
2022-07-05 18:21:01 +0300

    Add project files.

---
+++ RecapDemo2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.AccessControl;
+
+namespace RecapDemo2
+{
+    class Program
+    {
+        static void Main()
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Logger = new DatabseLogger();
+            customerManager.Add();
+            Console.ReadLine();
+        }
+    }
+    class CustomerManager
+     {
+         public ILogger Logger { get; set; }
+         public void Add()
+         {
+             Logger.Log();
+             Console.WriteLine("Customer added!");
+         }
+     }
+    class DatabseLogger : ILogger
+        {
+        public void Log()
+        {
+            Console.WriteLine("Logged to database!");
+        }
+        }
+    class FileLogger : ILogger
+        {
+        public void Log()
+        {
+            Console.WriteLine("Logged to file");
+        }
+        }
+    class SmsLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to sms");
+        }
+    }
+
+    interface ILogger
+        {
+            void Log();
+        }
+ }
